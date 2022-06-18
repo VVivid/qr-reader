dc7750125115c42abe13f3c8eb56c0983c02064a
Ou David <oudavid97@gmail.com>
2022-06-19 01:10:19 +0700

    update

--- index.js
+++ index.js
@@ -30,8 +30,6 @@ app.post("/webhook", async (req, resp) => {
         },
       );
       const filePath = data.data.result.file_path;
-      console.log(req.body.message)
-      console.log(data.data)
       const res = await axios.get(
         `https://api.telegram.org/file/bot${botToken}/${filePath}`,
         {
